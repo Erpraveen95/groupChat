2e10c0d5f81b20436f98436ed15ec40ab02f9a29
Urs-Praveen <work.erpraveen@gmail.com>
2023-04-29 21:22:07 +0530

    added login page frountend

---
+++ public/js/login.js
@@ -0,0 +1,19 @@
+const emailInput = document.getElementById('email')
+const passwordInput = document.getElementById('password')
+const form = document.getElementById('login-form')
+
+form.addEventListener("submit", login)
+
+async function login(e) {
+    try {
+        e.preventDefault();
+        const loginCredentials = {
+            email: emailInput.value,
+            password: passwordInput.value
+        }
+        const serverResponse = await axios.post("http://localhost:3000/login", loginCredentials)
+        console.log(serverResponse)
+    } catch (error) {
+        console.log(err)
+    }
+}
